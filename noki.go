e52bd07b87a585f95cdbae1c9536d1e8d4b7679b
rjbrunette <rjbrunette@gmail.com>
2014-10-11 14:58:06 -0400

    testfunction

--- noki.go
+++ noki.go
@@ -1,8 +1,16 @@
-package main 
+package main
 
-import("fmt")
+import (
+    "net/http" //package for http based web programs
+    "fmt"
+)
 
-func main(){
-   fmt.Println("Hello World!")
+func remindersHandler(w http.ResponseWriter, r *http.Request) { 
+    fmt.Println("Inside handler")
+    fmt.Fprintf(w, "Hello world from my Go program!")
+}
 
+func main() {
+    http.HandleFunc("/reminders", remindersHandler) // redirect all urls to the handler function
+    http.ListenAndServe("localhost:9000", nil) // listen for connections at port 9999 on the local machine
 }
