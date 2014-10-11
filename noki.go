491a91c159633bdbcf85b03d80b2d26da6196067
rjbrunette <rjbrunette@gmail.com>
2014-10-11 15:56:29 -0400

    rems

--- noki.go
+++ noki.go
@@ -3,21 +3,38 @@ package main
 import (
     "net/http" //package for http based web programs
     "fmt"
+    "encoding/json"
     "github.com/gorilla/mux"
 )
 
-func remindersHandler(w http.ResponseWriter, r *http.Request) { 
+type Reminder struct {
+	Date string
+	Type string
+}
+
+func remindersGetHandler(w http.ResponseWriter, r *http.Request) { 
     fmt.Println("Inside handler")
     fmt.Fprintf(w, "Hello world from my Go program!")
 }
+func remindersPostHandler(w http.ResponseWriter, r *http.Request) { 
+    decoder := json.NewDecoder(r.Body)
+    var r Reminder
+    err := decoder.Decode(&r)
+    if err != nil {
+        panic()
+    }   
+    fmt.Println(r)
+    fmt.Fprintf(w, "Hello world from my Go program!")
+}
+
 
 func main() {
 	r := mux.NewRouter()
-    r.HandleFunc("/", remindersHandler).
+    r.HandleFunc("/reminders", remindersGetHandler).
     	Methods("GET")
-    	
-    r.HandleFunc("/products", remindersHandler)
-    r.HandleFunc("/articles", remindersHandler)
+     r.HandleFunc("/reminders", remindersPostHandler).
+    	Methods("POST")
+
     http.Handle("/", r)
     http.ListenAndServe(":9000", nil)
 }
